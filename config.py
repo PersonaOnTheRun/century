6d39564d0965bd2277566284bb90fcde1240ee79
Cam <personaontherun@gmail.com>
2018-01-24 13:55:12 -0500

    updated link for lets encrypt

---
+++ config.py
@@ -0,0 +1,5 @@
+bind = '0.0.0.0:443'
+
+keyfile = 'privkey.pem'
+certfile = 'cert.pem'
+ca_certs = 'chain.pem'
--- static/js/terminal.js
+++ static/js/terminal.js
@@ -10,7 +10,7 @@ var typed = new Typed("#terminal-short-info", {
    * @property {array} strings strings to be typed
    * @property {string} stringsElement ID of element containing string children
    */ // Moved from the East to the West. Raised by the noughties.
-  strings: [ '<br>hi, my name is cam. i\'m a creator.'],
+  strings: [ '<br>hi, i\'m cam. i\'m a creator.'],
   stringsElement: null,
 
   /**
@@ -159,7 +159,38 @@ var typed = new Typed("#my-quote", {
    * @property {array} strings strings to be typed
    * @property {string} stringsElement ID of element containing string children
    */ // Moved from the East to the West. Raised by the noughties.
-  strings: ['<br>enjoy these inspiring quotes','<br>"If you always do what interests you, at least one person is pleased."', '<br>"It does not matter how slowly you go, so long as you do not stop."', '<br>"Opportunities don\'t happen. You create them."', '<br>"Try not to become a person of success, but rather try to become a person of value."','<br>"Failure is another steppingstone to greatness."',],
+  strings: ['<br>Chapter 1: Commodities',
+            '<br>Section 1: The Two Factors of a Commodity:',
+            '<br>Use-Value and Value',
+            '<br>The wealth of those societies in which the capitalist mode of production prevails, presents itself',
+            '<br>as “an immense accumulation of commodities,”',
+            '<br>1 its unit being a single commodity. Our investigation must therefore begin with the analysis of a commodity.',
+            '<br>A commodity is, in the first place, an object outside us, a thing that by its properties satisfies',
+            '<br>human wants of some sort or another. The nature of such wants, whether, for instance, they spring',
+            '<br>from the stomach or from fancy, makes no difference.',
+            '<br>2 Neither are we here concerned to know how the object satisfies these wants,',
+            '<br>whether directly as means of subsistence, or indirectly as means of production.',
+            '<br>Every useful thing, as iron, paper, &c., may be looked at from the two points of view of quality',
+            '<br>and quantity. It is an assemblage of many properties, and may therefore be of use in various ways.',
+            '<br>To discover the various uses of things is the work of history.3 So also is the establishment of',
+            '<br>socially-recognized standards of measure for the quantities of these useful objects. The diversity',
+            '<br>of these measures has its origin partly in the diverse nature of the objects to be measured, partly in convention.',
+            '<br>The utility of a thing makes it a use value.4 But this utility is not a thing of air. Being limited by',
+            '<br>the physical properties of the commodity, it has no existence apart from that commodity. A',
+            '<br>commodity, such as iron, corn, or a diamond, is therefore, so far as it is a material thing, a use',
+            '<br>value, something useful. This property of a commodity is independent of the amount of labour',
+            '<br>required to appropriate its useful qualities. When treating of use value, we always assume to be',
+            '<br>dealing with definite quantities, such as dozens of watches, yards of linen, or tons of iron. The use',
+            '<br>values of commodities furnish the material for a special study, that of the commercial knowledge',
+            '<br>of commodities.5 Use values become a reality only by use or consumption: they also constitute',
+            '<br>the substance of all wealth, whatever may be the social form of that wealth. In the form of society',
+            '<br>we are about to consider, they are, in addition, the material depositories of exchange value.',
+            '<br>Exchange value, at first sight, presents itself as a quantitative relation, as the proportion in which',
+            '<br>values in use of one sort are exchanged for those of another sort,6 a relation constantly changing',
+            '<br>with time and place. Hence exchange value appears to be something accidental and purely',
+            '<br>relative, and consequently an intrinsic value, i.e., an exchange value that is inseparably connected',
+            '<br>with, inherent in commodities, seems a contradiction in terms.7 Let us consider the matter a little',
+            '<br>more closely.', ],
   stringsElement: null,
 
 
