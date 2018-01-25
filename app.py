2d694574e24baa7be72008847aeec58cb221fc9d
Cam <personaontherun@gmail.com>
2018-01-24 19:05:49 -0500

    updates

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
-@app.route('/.well-known/acme-challenge/CBXExrtnDPGN25pq_QQ1M_jhBcNsiqkXhE20tpo6BgI',methods = ['GET'])
-def challenge():
-    return Response('CBXExrtnDPGN25pq_QQ1M_jhBcNsiqkXhE20tpo6BgI.zryhClOrau6WfUaEVTYYHJq8c9PqLf9TbjX3XUpOu8k', mimetype='text/plain')
-
-
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html')
